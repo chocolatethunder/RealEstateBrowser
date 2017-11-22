f43ef85e0dba03bb405a278768e18ab6c468ed1f
Saurabh <saurabh.tomar@gmail.com>
2017-11-22 16:46:45 -0700

    Initial Commit

--- RealEstateBrowser/MainPage.xaml.cs
+++ RealEstateBrowser/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +26,18 @@ namespace RealEstateBrowser
         public MainPage()
         {
             this.InitializeComponent();
+            /*TransitionCollection collection = new TransitionCollection();
+            NavigationThemeTransition theme = new NavigationThemeTransition();
+
+            var info = new ContinuumNavigationTransitionInfo();
+            theme.DefaultNavigationTransitionInfo = info;
+            collection.Add(theme);
+            this.Transitions = collection;*/
+        }
+
+        private void searchLocation_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(intro1));
         }
     }
 }
